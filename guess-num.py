ac1e18bb8e9d6b0d538db808880b7de8c7406f42
Millie.Chen <millie.chen.ck@gmail.com>
2021-07-25 22:21:23 +0800

    final version

--- guess-num.py
+++ guess-num.py
@@ -4,8 +4,12 @@
 # 猜錯的話, 要告訴他, 比答案大/小
 
 import random
+start = input('請決定隨機數字起始值: ')
+end = input('請決定隨機數字結束值: ')
+start = int(start)
+end = int(end)
 
-n = random.randint(1, 100)
+n = random.randint(start, end)
 count = 0   # 計數的變數
 while True:
     count += 1  # count = count + 1
